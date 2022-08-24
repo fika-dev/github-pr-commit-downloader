4ac61b2d09534b0bbcf6001db3b84503a6227845
Wonmo Jung <jungcro@gmail.com>
2022-08-24 14:04:49 +0900

    [test] parse_commits_from_pr

--- test_mac.py
+++ test_mac.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 from crawler import Crawler
-from tasks import visit
+from tasks import parse_commits_from_pr
 chrome_options = webdriver.ChromeOptions()
 # options.headless = False
 # chrome_options.add_argument('--headless')
@@ -9,4 +9,5 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 path = '/Users/wonmojung/.chrome_drivers/chromedriver_104'
 crawler = Crawler(options=chrome_options, path=path, crawler_id='test1', remote=False)
-visit(crawler.driver, "https://github.com/1milligram/frontend-tips/pull/164")
+commits = parse_commits_from_pr(crawler.driver, "1milligram", "frontend-tips", "164")
+print(len(commits))
