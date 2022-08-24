68282cb9c22ae31bea70ab6d4c99b6ac5b861524
Wonmo Jung <jungcro@gmail.com>
2022-08-24 15:53:38 +0900

    [add] request to github

--- github.py
+++ github.py
@@ -1,5 +1,12 @@
 from decouple import config
+import requests
+from requests import Response
 class Github:
   def __init__(self):
       self.token = config('GH_TOKEN')
-      
+  
+  def request(self, endpoint: str, params: dict = None)->Response:
+    return requests.get(endpoint, params=params, headers={
+      "Authorization": f'token {self.token}',
+      "Accept": "application/vnd.github+json",
+    })
