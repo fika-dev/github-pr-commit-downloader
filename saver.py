580fa249176f35943265dd408bdab8db9655e968
Wonmo Jung <jungcro@gmail.com>
2022-08-24 17:26:22 +0900

    [add] add_all to saver

--- saver.py
+++ saver.py
@@ -4,14 +4,14 @@ class Saver:
   def __init__(self, out_dir):
     self.ar = lmd.Archive(out_dir)
 
-  def __str__(self):
-      return self.crawler_id
-
   def save(self):
       self.ar.commit()
   
   def add(self, data):
     self.ar.add_data(data)
+
+  def add_all(self, data: list):
+    [self.ar.add_data(d) for d in data]
 
   def close(self):
       self.ar.commit()
