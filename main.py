81a0da2fe4d5c4fc0b57ce2a9972cce9d0da35f4
Wonmo Jung <jungcro@gmail.com>
2022-08-24 19:45:51 +0900

    [add] main & test_mac

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-from selenium import webdriver
-chrome_options = webdriver.ChromeOptions()
-# options.headless = False
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
+import os
+import pickle
+from github import Github
+from loader import Loader
+from saver import Saver
+from tqdm import tqdm
+ 
+from tasks import get_pr_list, parse_commits_from_pr_using_api
+
+
+github = Github()
+loader = Loader('data/repos_typescript_starts_100_pr_commit_limit.json')
+pr_saver = Saver('../data/pr')
+commit_saver = Saver('../data/commit')
+
+if 'repo_ckpt.pkl' in os.listdir():
+    with open('repo_ckpt.pkl', 'rb') as f:
+        repo_idx, pr_idx = pickle.load(f)
+    print(f"Loading from {repo_idx}..{pr_idx}")
+else:
+  repo_idx = 0
+  pr_idx = -1
+
+for i in  range(loader.repo_length):
+  if (i >= repo_idx):
+    repo = loader.get(i)
+    [owner, repo] = repo['name'].split('/')
+    pr_list = get_pr_list(github, owner, repo)
+    for pr_i, pr in tqdm(enumerate(pr_list)):
+      if (pr_i > pr_idx):
+        pr_saver.add(pr)
+        commits = parse_commits_from_pr_using_api(github, owner, repo, pr['number'], i, pr_i)
+        commit_saver.add_all(commits)
+        commit_saver.save()
+        pr_saver.save()
+
+commit_saver.close()
+pr_saver.close()
--- test_mac.py
+++ test_mac.py
@@ -1,13 +1,35 @@
 from selenium import webdriver
-
 from crawler import Crawler
-from tasks import parse_commits_from_pr
-chrome_options = webdriver.ChromeOptions()
-# options.headless = False
+from github import Github
+from loader import Loader
+from saver import Saver
+from tqdm import tqdm
+ 
+from tasks import get_pr_list, parse_commits_from_pr_using_api, parse_commits_from_pr_using_bs
+# chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-path = '/Users/wonmojung/.chrome_drivers/chromedriver_104'
-crawler = Crawler(options=chrome_options, path=path, crawler_id='test1', remote=False)
-commits = parse_commits_from_pr(crawler.driver, "1milligram", "frontend-tips", "164")
-print(len(commits))
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# path = '/Users/wonmojung/.chrome_drivers/chromedriver_104'
+# crawler = Crawler(options=chrome_options, path=path, crawler_id='test1', remote=False)
+
+
+
+github = Github()
+loader = Loader('data/repos_typescript_starts_100_pr_commit_limit.json')
+pr_saver = Saver('/Users/wonmojung/Fika/data/pr')
+commit_saver = Saver('/Users/wonmojung/Fika/data/commit')
+
+for i in  range(loader.repo_length):
+  repo = loader.get(i)
+  [owner, repo] = repo['name'].split('/')
+  pr_list = get_pr_list(github, owner, repo)
+  for pr in tqdm(pr_list):
+    pr_saver.add(pr)
+    commits = parse_commits_from_pr_using_api(github, owner, repo, pr['number'])
+    commit_saver.add_all(commits)
+    commit_saver.save()
+    pr_saver.save()
+
+commit_saver.close()
+pr_saver.close()
